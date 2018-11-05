fec0aa5b01c9fdc0683834365a37504520c7070f
lorenz <looooo@users.noreply.github.com>
2018-11-05 10:20:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 import os
+import subprocess as sub
 
 version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
                             "freecad", "plot", "version.py")
 with open(version_path) as fp:
     exec(fp.read())
+    
+# try to create a resource file
+proc = sub.Popen(["pyside-rcc", "-o", "Ship_rc.py", "resources/Ship.qrc"], stdout=sub.PIPE, stderr=sub.PIPE)
+out, err = proc.communicate()
+print(out.decode("utf8"))
+print(err.decode("utf8"))
 
 setup(name='freecad.plot',
       version=str(__version__),
