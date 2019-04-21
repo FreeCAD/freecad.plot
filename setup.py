bfc429d579f4060536d1e718bf304b6d6f1cb89d
Oliver Oxtoby <oliveroxtoby@gmail.com>
2019-04-21 10:32:26 +0200

    Compile resources if absent - so that can be installed by AddOn manager

--- setup.py
+++ setup.py
@@ -1,29 +1,13 @@
 from setuptools import setup
 import os
-import subprocess as sub
+from freecad.plot.PlotGui import CompileResources
 
 version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 
                             "freecad", "plot", "version.py")
 with open(version_path) as fp:
     exec(fp.read())
     
-# try to create a resource file
-# assume either pyside2-rcc or pyside-rcc are available.
-# if both are available pyside2-rcc is used.
-rc_input = os.path.abspath(os.path.join("freecad", "plot", "resources", "Plot.qrc"))
-rc_output = os.path.join("freecad", "plot", "Plot_rc.py")
-try:
-    try:
-        proc = sub.Popen(["pyside2-rcc", "-o", rc_output, rc_input], stdout=sub.PIPE, stderr=sub.PIPE)
-        out, err = proc.communicate()
-    except FileNotFoundError:
-        proc = sub.Popen(["pyside-rcc", "-o", rc_output, rc_input], stdout=sub.PIPE, stderr=sub.PIPE)
-        out, err = proc.communicate()
-except Exception as e:
-    print("An error occured while trying to create the resource file: \n" + str(e))
-
-print(out.decode("utf8"))
-print(err.decode("utf8"))
+CompileResources()
 
 setup(name='freecad.plot',
       version=str(__version__),
