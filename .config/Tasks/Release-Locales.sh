6fa9f116e3c7728e761b49bd5158b3ff2423e2e2
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2025-10-26 04:50:43 -0400

    Added EUO Pipefail to bash scripts

--- .config/Tasks/Release-Locales.sh
+++ .config/Tasks/Release-Locales.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 
 release='/usr/lib/qt6/bin/lrelease'
 
--- .config/Tasks/Update-Locales.sh
+++ .config/Tasks/Update-Locales.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 
 update='/usr/lib/qt6/bin/lupdate'
 
